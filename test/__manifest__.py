94e969bb7455d488e089d8002239474616d1d5ae
Alejandro Fidececchi <ale.fidececchi@gmail.com>
2024-05-15 18:00:17 -0300

    first commit

---
+++ test/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Test",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Alejandro",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        # 'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        # 'demo/demo_data.xml',
+    ],
+    'application': True,
+}
